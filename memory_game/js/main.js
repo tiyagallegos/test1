cdcf53f78796533bae7dfa4a5ca89d373610518a
tiyagallegos <60327779+tiyagallegos@users.noreply.github.com>
2020-02-04 06:09:27 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+
+
+ var cards = ["queen", "queen", "king", "king"];
+ var cardsInPlay = [];
+
+ var cardOne = [0];
+ cardsInPlay.push('cardOne');
+ var cardTwo = [1];
+ cardsInPlay.push('cardTwo');
+ var cardThree = [2];
+ cardsInPlay.push('cardThree');
+ vare cardFour = [3];
+ cardsInPlay.push('cardFour');
+ 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+
